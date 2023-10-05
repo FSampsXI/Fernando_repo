46c78e8ccbeda710d7c3fed0e05d5d2f8e30afba
FSampsXI <fernando.11ssampaio@gmail.com>
2023-10-05 16:51:14 -0300

    just a test

--- Desafios/Rodrigo/main.py
+++ Desafios/Rodrigo/main.py
@@ -5,7 +5,7 @@
 
 import csv
 
-def escrever_lista(name, home):
+def escreve_lista(name, home):
         with open("names.csv", "a", newline="") as file:
             writer = csv.DictWriter(file, fieldnames=["name", "home"])
             writer.writerow({"name": name, "home": home})
